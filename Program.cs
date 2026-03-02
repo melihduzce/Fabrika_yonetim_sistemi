db6d892e18051e33d0d5ad30dfcf29b1c510545f
yemrekaymak <yunsemrekaymak9@gmail.com>
2026-03-03 00:50:43 +0300

    siparis ve müsteri olusturuldu ,models kısmı geliştirildi,program.cs e CORS eklendi

--- Program.cs
+++ Program.cs
@@ -3,22 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 builder.Services.AddControllers()
     .AddJsonOptions(options => 
         options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
+        
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // CORS İZİN BELGESİ (Dışarıdan gelen isteklere kapıyı açar)
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("HerkesGelsin", builder =>
+    options.AddPolicy("HerkesGelsin", policyBuilder =>
     {
-        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
 
@@ -29,8 +29,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors("HerkesGelsin"); // Güvenliğe bu izni uygula diyoruz
+
+// Güvenliğe bu izni uygula diyoruz (Şalter kalktı!)
+app.UseCors("HerkesGelsin"); 
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
